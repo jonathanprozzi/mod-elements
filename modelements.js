465967ec0cead1ec1a3e53b22495592cd5455323
Jonathan Prozzi <jonathanprozzi@gmail.com>
2016-06-16 17:28:30 -0400

    changed some html and added some intructions to the .js file

---
+++ modelements.js
@@ -0,0 +1,18 @@
+/* ****             READ ME!!               ****
+
+    welcome to the javascript file for the modifying/adding/removing elements lesson! there is lots of html in the index.html file but don't fret! you don't need to worry about a lot of it.
+        this activity is intended to be a (hopefully!) fun way to practice working with     html elements and dom selection techniques.
+        primarily you'll be using .getElementById('id-name') such as this:
+
+        var myVar = document.getElementById('id-name'); and then you'll use javascript to modify and change whichever element you're targetting.
+
+        you can also do:
+            document.body.removeChild(myVar) and document.body.appendChild(myVar)
+                these remove and add targetted elements
+
+    throughout this file there will be some tips to guide you along the way.
+
+    remember: this is super important! please don't remove things from the index.html file! this activity is specifically for modifying elements -with- javascript!
+
+    you can delete these comments after reading. (nless you really like them!)
+*/
